bced3e7a22acf424993e58b0a03acefe7b71b7db
sotopro <dasg.1993@gmail.com>
2022-08-13 10:41:39 -0500

    [setup] - add fonts add dependencies for navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { useFonts } from "expo-font";
+import { ActivityIndicator } from "react-native";
+
 import Categories from "./screens/categories";
 
 export default function App() {
+  const [loaded] = useFonts({
+    "Lato-Regular": require("../assets/fonts/Lato-Regular.ttf"),
+    "Lato-Bold": require("../assets/fonts/Lato-Bold.ttf"),
+    "Lato-Light": require("../assets/fonts/Lato-Light.ttf"),
+    "Lato-Black": require("../assets/fonts/Lato-Black.ttf"),
+    "Lato-Thin": require("../assets/fonts/Lato-Thin.ttf"),
+    "Lato-Italic": require("../assets/fonts/Lato-Italic.ttf"),
+  });
+
+  if (!loaded) {
+    return <ActivityIndicator size="large" color="#0000ff" />;
+  }
   return <Categories />;
 }
