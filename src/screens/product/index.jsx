f545df80c734ac159b2caaa4350f5099af2d0761
sotopro <dasg.1993@gmail.com>
2022-08-13 13:42:56 -0500

    [feature] - product detail

--- src/screens/product/index.jsx
+++ src/screens/product/index.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Text, View } from "react-native";
 
+import { PRODUCTS } from "../../constants/data/products";
 import { styles } from "./styles";
 
-const Product = () => {
+const Product = ({ navigation, route }) => {
+  const { productId } = route.params;
+  const product = PRODUCTS.find((product) => product.id === productId);
   return (
     <View style={styles.container}>
-      <Text>Product</Text>
+      <Text style={styles.name}>{product.name}</Text>
+      <Text style={styles.description}>{product.description}</Text>
+      <Text style={styles.price}>$ {product.price.toFixed(2)}</Text>
+      <Text style={styles.weight}>Peso: {product.weight}</Text>
     </View>
   );
 };
