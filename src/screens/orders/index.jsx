9c9b4ba02a9c291a7431234ed83088b60cc1f097
sotopro <dasg.1993@gmail.com>
2022-08-27 12:19:24 -0500

    [feature] - implement get and delete orders

--- src/screens/orders/index.jsx
+++ src/screens/orders/index.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import { useFocusEffect } from "@react-navigation/native";
+import React, { useEffect, useCallback } from "react";
 import { View, FlatList } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
 
 import { OrderItem } from "../../components";
-import { ORDERS } from "../../constants/data/orders";
+import { deleteOrder, getOrders } from "../../store/actions/orders.action";
 import { styles } from "./styles";
 
 const Orders = () => {
-  const items = ORDERS;
+  const dispatch = useDispatch();
+  const orders = useSelector((state) => state.order.orders);
+  const items = orders;
 
   const onHandleDelete = (id) => {
-    console.warn(`Delete ${id}`);
+    dispatch(deleteOrder(id));
   };
 
   const keyExtractor = (item) => item.id.toString();
 
   const renderItem = ({ item }) => <OrderItem item={item} onDelete={onHandleDelete} />;
+  useFocusEffect(
+    useCallback(() => {
+      dispatch(getOrders());
+    }, [dispatch])
+  );
+
   return (
     <View style={styles.container}>
       <View style={styles.orderList}>
